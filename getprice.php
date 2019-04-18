1a3d5530a717b5ccfc5f59d88dedbc6a30f746bc
f00d4tehg0dz <adrianvfx@gmail.com>
2019-04-18 09:51:00 -0400

    Delete getprice.php

--- getprice.php
+++
@@ -1,7 +0,0 @@
-<?php include "connection.php";
-
-$a="SELECT * FROM category WHERE AND cat_id = '".$_POST["CategoryID"]."'";
-$q=mysqli_query($db, $a);
-while ($record=mysqli_fetch_array($q)) {
-    echo $record['cat_name'];
-}
